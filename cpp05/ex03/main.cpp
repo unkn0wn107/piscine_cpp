0a2629f5c2721044a8d1bd4d7869fa076abfec13
agaley <agaley@student.42lyon.fr>
2024-05-16 13:43:51 +0200

    [cpp05ex03] Add Intern to main

--- cpp05/ex03/main.cpp
+++ cpp05/ex03/main.cpp
@@ -6,25 +6,25 @@
 /*   By: agaley <agaley@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/12 11:59:53 by agaley            #+#    #+#             */
-/*   Updated: 2024/05/15 16:16:41 by agaley           ###   ########lyon.fr   */
+/*   Updated: 2024/05/16 13:42:53 by agaley           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "PresidentialPardonForm.hpp"
-#include "RobotomyRequestForm.hpp"
-#include "ShrubberyCreationForm.hpp"
+#include "Intern.hpp"
 
 int main() {
+	Intern internus;
   Bureaucrat minimus("Minimus", 150);
 
   try {
     Bureaucrat             caesar("Julius Caesar", 25);
     Bureaucrat             marcus("Marcus Aurelius", 5);
-    PresidentialPardonForm ppf("Minimus");
+    AForm* ppf;
+	  ppf = internus.makeForm("presidential pardon", "Brutus");
 
-    std::cout << ppf << std::endl;
-    caesar.signForm(ppf);
-    marcus.executeForm(ppf);
+    std::cout << *ppf << std::endl;
+    caesar.signForm(*ppf);
+    marcus.executeForm(*ppf);
   } catch (const std::exception& e) {
     std::cout << "Exception caught: " << e.what() << std::endl;
   }
@@ -34,11 +34,12 @@ int main() {
   try {
     Bureaucrat            marcus("Plebeius Minimus", 140);
     Bureaucrat            caesar("Gaius Meridius", 137);
-    ShrubberyCreationForm scf("home");
+    AForm* scf;
+    scf = internus.makeForm("shrubbery creation", "Shrubberitus");
 
-    std::cout << scf << std::endl;
-    marcus.signForm(scf);
-    caesar.executeForm(scf);
+    std::cout << *scf << std::endl;
+    marcus.signForm(*scf);
+    caesar.executeForm(*scf);
   } catch (const std::exception& e) {
     std::cout << "Exception caught: " << e.what() << std::endl;
   }
@@ -48,13 +49,14 @@ int main() {
   try {
     Bureaucrat          maximus("Maximus Decimus Meridius", 72);
     Bureaucrat          plebeius("Plebeius Maximus", 45);
-    RobotomyRequestForm rrf("Asterix");
+    AForm *rrf;
+    rrf = internus.makeForm("robotomy request", "Robotomitus");
 
-    std::cout << rrf << std::endl;
-    minimus.signForm(rrf);
-    maximus.signForm(rrf);
-    minimus.executeForm(rrf);
-    plebeius.executeForm(rrf);
+    std::cout << *rrf << std::endl;
+    minimus.signForm(*rrf);
+    maximus.signForm(*rrf);
+    minimus.executeForm(*rrf);
+    plebeius.executeForm(*rrf);
   } catch (const std::exception& e) {
     std::cout << "Exception caught: " << e.what() << std::endl;
   }
