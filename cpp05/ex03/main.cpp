111a7d4634198cefee9b1a35e650c55b503a5fb2
agaley <agaley@student.42lyon.fr>
2024-05-17 14:32:15 +0200

    [cpp05ex03] format

--- cpp05/ex03/main.cpp
+++ cpp05/ex03/main.cpp
@@ -6,21 +6,20 @@
 /*   By: agaley <agaley@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/12 11:59:53 by agaley            #+#    #+#             */
-/*   Updated: 2024/05/16 13:42:53 by agaley           ###   ########lyon.fr   */
+/*   Updated: 2024/05/17 14:31:42 by agaley           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Intern.hpp"
 
 int main() {
-	Intern internus;
+  Intern internus;
   Bureaucrat minimus("Minimus", 150);
 
   try {
     Bureaucrat             caesar("Julius Caesar", 25);
     Bureaucrat             marcus("Marcus Aurelius", 5);
-    AForm* ppf;
-	  ppf = internus.makeForm("presidential pardon", "Brutus");
+    AForm* ppf = internus.makeForm("presidential pardon", "Brutus");
 
     std::cout << *ppf << std::endl;
     caesar.signForm(*ppf);
@@ -34,8 +33,7 @@ int main() {
   try {
     Bureaucrat            marcus("Plebeius Minimus", 140);
     Bureaucrat            caesar("Gaius Meridius", 137);
-    AForm* scf;
-    scf = internus.makeForm("shrubbery creation", "Shrubberitus");
+    AForm* scf = internus.makeForm("shrubbery creation", "Shrubberitus");
 
     std::cout << *scf << std::endl;
     marcus.signForm(*scf);
@@ -49,8 +47,7 @@ int main() {
   try {
     Bureaucrat          maximus("Maximus Decimus Meridius", 72);
     Bureaucrat          plebeius("Plebeius Maximus", 45);
-    AForm *rrf;
-    rrf = internus.makeForm("robotomy request", "Robotomitus");
+    AForm* rrf = internus.makeForm("robotomy request", "Robotomitus");
 
     std::cout << *rrf << std::endl;
     minimus.signForm(*rrf);
